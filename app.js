09222b213e9fe9a30d87360c7b41417da12d92fb
bernardofe <bernarddoferreira@outlook.com>
2022-07-25 20:21:54 -0300

    restore test

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("aplicação está se conectado ao banco de dados")
 // mais um console log de teste
 console.log("O paulo fez esse commit sem você ver na aula!")
 console.log("Enviando para o github")
+console.log("teste teste")
